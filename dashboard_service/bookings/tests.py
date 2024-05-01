fbeac16ebc93a4299d7d8456ee7b295905696cc0
Fauzan Nadhif <fauzannadhifm98@gmail.com>
2024-05-01 14:39:44 +0700

    add unit test

--- dashboard_service/bookings/tests.py
+++ dashboard_service/bookings/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Booking
+
+
+class BookingListAPIViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        Booking.objects.create(hotel_id=1, year=2024, month=5, daily_bookings=json.dumps({"1": 1, "2": 2}))
+
+    def test_booking_list_monthly(self):
+        url = "%s?year=2024&hotel_id=1&period=month" % (reverse("dashboards"))
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_body = response.json()
+        monthly_booking = response_body.get("monthly_bookings", [])
+        self.assertEqual(len(monthly_booking), 1)
+    
+    def test_booking_list_daily(self):
+        url = "%s?year=2024&hotel_id=1&period=day" % (reverse("dashboards"))
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_body = response.json()
+        daily_booking = response_body.get("daily_bookings", [])
+        self.assertEqual(len(daily_booking), 1)
+        self.assertEqual(len(daily_booking[0]["bookings"]), 2)
--- data_provider/events/tests.py
+++ data_provider/events/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Event
 
-# Create your tests here.
+
+class EventListCreateAPIViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_event_create(self):
+        url = reverse('events')
+
+        data = {
+            "hotel_id": 1,
+            "timestamp": "2024-04-30T12:37:00Z",
+            "rpg_status": 1,
+            "room_id": 1,
+            "night_of_stay": "2020-01-01"
+        }
+
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Event.objects.filter(hotel_id=1).exists())
+
+    def test_event_list(self):
+        url = reverse('events')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
